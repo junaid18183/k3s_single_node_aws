1e6a311a892329655e8786068d4b0fba6dafc9a2
Juned Memon <junaid18183@gmail.com>
2022-08-15 16:30:17 +0530

    added outpu

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,13 @@ output "test-instance-ip" {
 output "install_script_location" {
   value = "/var/lib/cloud/instance/scripts/install_k3s.sh"
 }
+
+output "node_joining_token" {
+  value = "/var/lib/rancher/k3s/server/node-token"
+}
+
+
+output "kubeconfig_file_for_remote_access" {
+  value = "/tmp/kube_config.yaml"
+}
+
