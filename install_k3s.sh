cbea2a34f9096274f92c748842511967cfaaf96e
Juned Memon <junaid18183@gmail.com>
2025-11-25 13:14:14 +0530

    updated script in 2025

--- install_k3s.sh
+++ install_k3s.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
+amazon-linux-extras enable selinux-ng
+yum clean metadata
+yum install selinux-policy-targeted container-selinux
+
 export PUBLIC_IP=$(curl ifconfig.me)
-K3S_VERSION=v1.27.6+k3s1
+K3S_VERSION=v1.31.14+k3s1
 echo $PUBLIC_IP
 curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=$K3S_VERSION INSTALL_K3S_EXEC="server --disable=traefik  --node-external-ip=$PUBLIC_IP --tls-san=$PUBLIC_IP --tls-san=k8s.ijuned.com" sh -
 mkdir -p /root/.kube/
